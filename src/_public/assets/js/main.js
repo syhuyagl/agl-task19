aa4cfd705ff3ffc37ee9ff15c91054950d3d8ce1
AGL_Hồ Sỹ Huy  (フイ) <ho_sy_huy@allgrow-labo.jp>
2022-12-20 15:53:39 +0700

    intro

--- src/_public/assets/js/main.js
+++ src/_public/assets/js/main.js
@@ -3,7 +3,7 @@ $(document).ready(function(){
         fade: true,
         speed: 1100,
         infinite: true,
-        autoplay: false,
+        autoplay: true,
         cssEase: "linear",
         arrows: false,
         dots: false,
