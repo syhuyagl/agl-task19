0c516ff99aa3732965238d59f1aa7ede41f4c30d
AGL_Hồ Sỹ Huy  (フイ) <ho_sy_huy@allgrow-labo.jp>
2022-12-22 11:19:33 +0700

    tab nav

--- src/_public/assets/js/main.js
+++ src/_public/assets/js/main.js
@@ -8,6 +8,24 @@ $(document).ready(function () {
     arrows: false,
     dots: false,
   });
+  function disableScroll() {
+    var ycoord = $(window).scrollTop();
+    $(".c-header__overlay").data("ycoord", ycoord);
+    ycoord = ycoord * -1;
+    $("body")
+      .css("position", "fixed")
+      .css("left", "0px")
+      .css("right", "0px")
+      .css("top", ycoord + "px");
+  }
+  function enableScroll() {
+    $("body")
+      .css("position", "")
+      .css("left", "auto")
+      .css("right", "auto")
+      .css("top", "auto");
+    $(window).scrollTop($(".c-header__overlay").data("ycoord"));
+  }
   $(".c-intro__content1").addClass("c-intro__content--active");
   let tab1 = $(".c-intro__tab1");
   let tab2 = $(".c-intro__tab2");
@@ -25,6 +43,20 @@ $(document).ready(function () {
       tab1.removeClass("c-intro__tab--active");
       $(".c-intro__content2").addClass("c-intro__content--active");
       $(".c-intro__content1").removeClass("c-intro__content--active");
+    }
+  });
+  $(".c-header__menu").click(function () {
+    if($('.c-header__open').hasClass('c-header__icon--active')){
+      $('.c-header__open').removeClass('c-header__icon--active')
+      $('.c-header__close').addClass('c-header__icon--active')
+      $('.c-header__overlay').addClass('c-header__overlay--active')
+      disableScroll()
+    }
+    else {
+      $('.c-header__open').addClass('c-header__icon--active')
+      $('.c-header__close').removeClass('c-header__icon--active')
+      $('.c-header__overlay').removeClass('c-header__overlay--active')
+      enableScroll()
     }
   });
 });
